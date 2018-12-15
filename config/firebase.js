d603da7b7f033940cd203f4ab42768a26afac429
bkhan23544 <bkhan23544@gmail.com>
2018-12-15 09:05:46 +0500

    first commit

---
+++ config/firebase.js
@@ -0,0 +1,12 @@
+  import * as firebase from "firebase"
+   // Initialize Firebase
+   var config = {
+    apiKey: "AIzaSyCrE3l5wZ8RVby8EvhmSI0UZ-Em9c9aoN8",
+    authDomain: "q-app-471e2.firebaseapp.com",
+    databaseURL: "https://q-app-471e2.firebaseio.com",
+    projectId: "q-app-471e2",
+    storageBucket: "q-app-471e2.appspot.com",
+    messagingSenderId: "464897911423"
+  };
+  firebase.initializeApp(config);
+  export default firebase;
